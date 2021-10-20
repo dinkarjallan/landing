6af73a94a7b9d43adc786e2968edd834db5fed80
Dinkar Jallan <dinkarjallan@gmail.com>
2021-10-20 14:12:53 +0530

    bug fixed
    fixed all responsiveness issues

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import './Main.css';
 import ServiceSection from './ServiceSection';
-// import Carousel from './Carousel';
 import ContactSection from './ContactSection';
 
 const Main = () => {
     return (
         <main className="main">
             <ServiceSection />
-            {/* <Carousel /> */}
             <ContactSection />
         </main>
     )
